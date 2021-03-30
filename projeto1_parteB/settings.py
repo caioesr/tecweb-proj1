6119bb5f600c01890c28680dd1d66d90eaac8309
caioesr <caioesr@al.insper.edu.br>
2021-03-30 10:27:05 -0300

    trollou

--- projeto1_parteB/settings.py
+++ projeto1_parteB/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,15 +84,23 @@ WSGI_APPLICATION = 'projeto1_parteB.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'proj1',
+#         'USER': 'admin',
+#         'PASSWORD': 'proj1tecweb',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'proj1',
-        'USER': 'admin',
-        'PASSWORD': 'proj1tecweb',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://localhost/proj1?user=admin&password=proj1tecweb',
+        conn_max_age=600,
+        ssl_require=not DEBUG
+    )
 }
 
 # Password validation
